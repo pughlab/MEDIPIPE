7a714bf48c73921a453c18220368226eaf9f6a9c
Yong <yzeng@uhnresearch.ca>
2022-02-02 17:00:06 -0500

    v1

--- build_reference_index.sh
+++ build_reference_index.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ## the script will build BWA index for combined human and spike-in genomes.
-## "Usage: ./download_build_reference.sh [GENOME] [SPIKEIN_FA] [INDEX_PREFIX] [DEST_DIR]"
-## "Example: ./download_build_reference.sh hg38 ./data/BAC_F19K16_F24B22.fa hg38_BAC_F19K16_F24B22 /your/genome/data/path/hg38"
+## "Usage: ./build_reference_index.sh [GENOME] [SPIKEIN_FA] [INDEX_PREFIX] [DEST_DIR]"
+## "Example: ./build_reference_index.sh hg38 ./data/BAC_F19K16_F24B22.fa hg38_BAC_F19K16_F24B22 /your/genome/data/path/hg38"
 
+## "Example: ./build_reference_index.sh hg38 ./data/BAC_F19K16_F24B22.fa hg38_BAC_F19K16_F24B22 /cluster/projects/tcge/DB/cfmedip-seq-pepeline/hg38"
 
 #################
 ## initilizaiton
@@ -13,37 +14,37 @@ source ~/miniconda3/etc/profile.d/conda.sh
 # OR:
 # source ~/conda3/etc/profile.d/conda.sh
 
-TSV=${DEST_DIR}/${GENOME}.tsv
+GENOME=$1
+SPIKEIN_FA=$2
+INDEX_PREFIX=$3
+DEST_DIR=$4
 
 ## human genome
+TSV=${DEST_DIR}/${GENOME}.tsv
 hg_fa=` awk '{if($1=="ref_fa") print $2}' $TSV`
 
-
-## combine genSPIKEIN_FAomes
-
-cat ${hg_fa} ${SPIKEIN_FA} > ${DEST_DIR}/${hg38_BAC_F19K16_F24B22}.fa
+## combine human genome with sipke-in sequence
+cat ${hg_fa} ${SPIKEIN_FA} > ${DEST_DIR}/${INDEX_PREFIX}.fa
 
 
 #################################################
 ## build bwa index for combined reference genomes
 #################################################
-## combine genomes
-cat $(basename ${REF_FA}) TAIR10_chr_all.fas.gz > ${GENOME}_tair10.fa.gz
 
 
 ## Might need to build index separatly due to the storage limitation
-## in login homefolder (50G quota for H4H)
+cd ${DEST_DIR}
 
 echo "=== Building bwa index for mereged genomes ..."
 conda activate tcge-cfmedip-seq-pipeline
 
-#bwa index -a bwtsw ${GENOME}_tair10.fa
+bwa index -a bwtsw ${INDEX_PREFIX}.fa
 
-#mkdir -p bwa_index_${GENOME}_tair10
-#mv ${GENOME}_tair10* ./bwa_index_${GENOME}_tair10
+mkdir -p bwa_index_${INDEX_PREFIX}
+mv ${INDEX_PREFIX}.* ./bwa_index_${INDEX_PREFIX}
 
-## bwa index merged
-#bwa_idx_merged=$(ls $PWD/bwa_index_${GENOME}_tair10/*fa)
-#echo -e "bwa_idx_hg_tair\t${bwa_idx_merged}" >> ${TSV}
+## adding new bwa index into manifest file
+bwa_idx_merged=$(ls $PWD/bwa_index_${INDEX_PREFIX}/*fa)
+echo -e "bwa_idx_hg_spikein\t${bwa_idx_merged}" >> ${TSV}
 
 echo "=== Done! ==="
